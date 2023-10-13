3794a3886f2a873da94e300a93c9203620e7a98f
Jorge Madrigal <jorge.madrigalsolano81@gmail.com>
2023-10-12 19:23:58 -0600

    test

--- Sistema Control de Asistencia Cervantes/Migrations/20230915040123_NombreDeLaMigracion.Designer.cs
+++ Sistema Control de Asistencia Cervantes/Migrations/20230915040123_NombreDeLaMigracion.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Sistema_Control_de_Asistencia_Cervantes.Dominio;
@@ -11,9 +12,11 @@ using Sistema_Control_de_Asistencia_Cervantes.Dominio;
 namespace SistemaControldeAsistenciaCervantes.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20230915040123_NombreDeLaMigracion")]
+    partial class NombreDeLaMigracion
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
